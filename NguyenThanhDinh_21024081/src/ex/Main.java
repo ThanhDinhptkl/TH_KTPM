f1a16689de2ad25f35bdddafca2bdaef982f59e6
Nguyen Thanh Dinh <125951611+ThanhDinhptkl@users.noreply.github.com>
2025-03-01 08:42:09 +0700

    update main and add ex2

--- NguyenThanhDinh_21024081/src/ex/Main.java
+++ NguyenThanhDinh_21024081/src/ex/Main.java
@@ -6,5 +6,7 @@ public class Main {
 		System.out.println("EagerInitializedSingleton: " + singletonInstance.hashCode());
 		StaticBlockSingleton singletonInstance1 = StaticBlockSingleton.getInstance();
 		System.out.println("StaticBlockSingleton: " + singletonInstance1.hashCode());
+		LazyInitializedSingleton singletonInstance2 = LazyInitializedSingleton.getInstance();
+		System.out.println("LazyInitializedSingleton: " + singletonInstance2.hashCode());
 	}
 }
